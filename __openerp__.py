07723853302f27214d693023a65014c9f98e85fb
Bernard Delhez <bernarddelhez@gmail.com>
2015-04-14 14:46:23 +0200

    Add description

--- __openerp__.py
+++ __openerp__.py
@@ -1,10 +1,11 @@
-﻿{
+{
     'name': 'Report Download Name',
     'version': '1.0',
     'depends': ['report'],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'Report',
+    'description': 
     """
     It sets the PDF name correctly when downloading report.
     It generate the name by evaluating the attachment field of a report declaration.
@@ -18,4 +19,5 @@
     This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.    
     This module is inspired by the module from Nicolas Brisac: https://github.com/oasiswork/odoo-report-download-name/
     """,
+    'data': [],
 }
